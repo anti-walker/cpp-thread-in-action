bcce1181ec814b6068ecdc1da6c3461d77ce9df1
anti-walker <dongshucai@126.com>
2018-11-18 18:26:05 +0800

    move stack into hpp and add queue

---
+++ thread_safe_queue.hpp
@@ -0,0 +1,58 @@
+#include <queue>
+#include <memory>
+#include <mutex>
+#include <condition_variable>
+#include <utility>
+
+template<class T>
+class threadsafe_queue
+{
+private:
+  std::queue<T> _data_queue;
+  std::mutex _m;
+  std::condition_variable _data_cond;
+
+public:
+  threadsafe_queue()
+  {
+
+  }
+
+  threadsafe_queue(const threadsafe_queue& r) = delete;
+
+  void push(const T& value)
+  {
+    std::lock_guard<std::mutex> lock(_m);
+    _data_queue.push(value);
+    _data_cond.notify_one();
+  }
+
+  void wait_and_pop(T& value)
+  {
+    std::lock_guard<std::mutex> lock(_m);
+    _data_cond.wait(lock, [this]{
+      return !_data_queue.empty();
+    });
+
+    value = std::move(_data_queue.front());
+    _data_queue.pop();
+  }
+
+  bool try_pop(T& value)
+  {
+    std::lock_guard<std::mutex> lock(_m);
+    if (_data_queue.empty()) {
+      return false;
+    }
+
+    value = std::move(_data_queue.front());
+    _data_queue.pop();
+    return true;
+  }
+
+  bool empty()
+  {
+    std::lock_guard<std::mutex> lock(_m);
+    return _data_queue.empty();
+  }
+};
---
+++ thread_safe_stack.hpp
@@ -0,0 +1,69 @@
+#include <exception>
+#include <stack>
+#include <mutex>
+#include <memory>
+
+
+class empty_stack : public std::exception
+{
+  const char* what() const throw();
+};
+
+const char* empty_stack::what() const throw()
+{
+  return "empty in stack";
+}
+
+template<class T>
+class threadsafe_stack
+{
+private:
+  std::stack<T> _data;
+  std::mutex _m;
+
+public:
+  threadsafe_stack() {}
+  threadsafe_stack(const threadsafe_stack& r)
+  {
+    std::lock_guard<std::mutex> lock(_m);
+    _data = r._data;
+  }
+
+  threadsafe_stack& operator = (const threadsafe_stack& ) = delete;
+
+  void push(const T& value)
+  {
+    std::lock_guard<std::mutex> lock(_m);
+    _data.push(value);
+  }
+
+  std::shared_ptr<T> pop()
+  {
+    std::lock_guard<std::mutex> lock(_m);
+    if (_data.empty())
+    {
+      throw empty_stack();
+    }
+    std::shared_ptr<T> value(std::make_shared<T>(_data.top()));
+    _data.pop();
+
+    return value;
+  }
+
+  void pop(T& value)
+  {
+    std::lock_guard<std::mutex> lock(_m);
+    if (_data.empty())
+    {
+      throw empty_stack();
+    }
+    value = _data.top();
+    _data.pop();
+  }
+
+  bool empty() const
+  {
+    std::lock_guard<std::mutex> lock(_m);
+    return _data.empty();
+  }
+};
